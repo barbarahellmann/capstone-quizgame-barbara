059a3dac2382eb65816924f90868142078d912e3
barbarahellmann <babshellmann@web.de>
2024-05-08 10:09:35 +0200

    Test for getAllQuestions added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,11 @@ import './App.css'
 
 function App() {
 
+
+
     return (
         <>
-
+<h1>Hello</h1>
         </>
     )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,8 @@
-import {BrowserRouter} from "react-router-dom";
-import App from './App.tsx'
-import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import App from './App.tsx'
+import './index.css'
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
