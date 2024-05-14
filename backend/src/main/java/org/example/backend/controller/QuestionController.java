313fc55adaec50d034d5c7a736492c3a466dd7cc
barbarahellmann <babshellmann@web.de>
2024-05-14 16:17:34 +0200

    Integrated Idea from Basti du add ResponseEntity to Unit Test for Delete'

--- backend/src/main/java/org/example/backend/controller/QuestionController.java
+++ backend/src/main/java/org/example/backend/controller/QuestionController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.example.backend.dto.QuestionDTO;
 import org.example.backend.model.Question;
 import org.example.backend.service.QuestionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,8 +33,19 @@ public class QuestionController {
     }
 
 
+    //   @DeleteMapping("/{id}")
+    //   public String deleteQuestion(@PathVariable String id) {
+    //       return service.deleteQuestionById(id);
+    //   }
+
     @DeleteMapping("/{id}")
-    public String deleteQuestion(@PathVariable String id) {
-        return service.deleteQuestionById(id);
+    public ResponseEntity<String> deleteQuestion(@PathVariable String id) {
+        boolean deleted = Boolean.parseBoolean(service.deleteQuestionById(id));
+
+        if (deleted) {
+            return ResponseEntity.status(HttpStatus.OK).body("Question mit der ID: " + id + " erfolgreich gelöscht!");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Question mit der ID: " + id + " nicht gefunden.");
+        }
     }
 }
