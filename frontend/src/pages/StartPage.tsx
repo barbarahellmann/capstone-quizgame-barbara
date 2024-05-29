4d6506aea51824ed8d4aa390df1ecdc82c849dd9
barbarahellmann <babshellmann@web.de>
2024-05-29 15:02:37 +0200

    Removed Dummy Button

--- frontend/src/pages/StartPage.tsx
+++ frontend/src/pages/StartPage.tsx
@@ -1,13 +1,11 @@
-import {Link, useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 export default function StartPage() {
-    const nav = useNavigate();
 
 
     return (
             <h2>
                 <Link to="/play">Quiz starten</Link>
-                <button onClick={() => nav("/admin")}>Klick Mich</button>
             </h2>
     )
 }
