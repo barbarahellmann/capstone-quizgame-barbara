954e7a680a3f9a68498d6241fc44a3d12145b2d3
barbarahellmann <babshellmann@web.de>
2024-06-04 21:34:09 +0200

    Added Favicon

--- frontend/src/pages/PlayResult.tsx
+++ frontend/src/pages/PlayResult.tsx
@@ -1,31 +1,21 @@
 import {useLocation, useNavigate} from 'react-router-dom';
-import {Box, Button, Modal, Typography} from '@mui/material';
-import React, {useState} from "react";
+import {Box, Button, Typography} from '@mui/material';
+
 
 export default function PlayResult({user}: { user?: string }) {
     const location = useLocation();
     const {correctCount} = location.state || {correctCount: 0};
     const navigate = useNavigate();
 
-    const [modalOpen, setModalOpen] = useState(false);
-    const [modalMessage, setModalMessage] = useState('');
-
     const handleLogin = () => {
         const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080' : window.location.origin;
-        setModalMessage("Du wirst angemeldet.");
-        setModalOpen(true);
         setTimeout(() => {
             window.open(host + '/oauth2/authorization/github', '_self');
         }, 1000);
     };
 
-    // Nach dem ersten Durchlauf beim Quiz muss man sich einloggen, Button Text wird im Frontend angepasst
-    const handlePlayAgain = () => {
-        if (user !== "anonymousUser") {
-            navigate('/play');
-        } else {
-            handleLogin();
-        }
+    const handleStartNewQuiz = () => {
+        navigate('/play');
     };
 
     return (
@@ -40,32 +30,17 @@ export default function PlayResult({user}: { user?: string }) {
             <Typography variant="body1" sx={{color: '#FFFFFF', marginBottom: 4}}>
                 Du hast {correctCount} von 5 Fragen richtig beantwortet.
             </Typography>
-            <Button variant="contained" color="secondary" onClick={handlePlayAgain} sx={{marginBottom: 2}}>
-                {user === "anonymousUser" ? 'Einloggen' : 'Quiz neu starten'}
-            </Button>
-            <Modal
-                open={modalOpen}
-                onClose={() => setModalOpen(false)}
-                aria-labelledby="modal-title"
-                aria-describedby="modal-description"
-            >
-                <Box sx={{
-                    position: 'absolute',
-                    top: '50%',
-                    left: '50%',
-                    transform: 'translate(-50%, -50%)',
-                    width: 400,
-                    bgcolor: 'background.paper',
-                    border: '2px solid #000',
-                    boxShadow: 24,
-                    p: 4,
-                }}>
-                    <Typography id="modal-title" variant="h6" component="h2">
-                        Hinweis
-                    </Typography>
-                    <Typography id="modal-description" sx={{mt: 2}}>
-                        {modalMessage}
-                    </Typography>
+            {user === "anonymousUser" ? (
+                <div>
+                    <p>Logge dich ein, wenn du noch einmal Spielen möchtest.</p>
+                    <Button variant="contained" color="secondary" onClick={handleLogin}
+                    >Login</Button>
+                </div>
+            ) : (
+                user && <Button variant="contained" color="secondary" onClick={handleStartNewQuiz}
+                >Neues Quiz starten</Button>
+
+            )}
         </Box>
     );
 }
